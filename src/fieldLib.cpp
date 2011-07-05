35da93d11a64828ff4427557e31b3fe2ea61283c
damicha <damicha>
2011-07-05 21:50:21 +0200

    field size was changed to 7x5

--- src/fieldLib.cpp
+++ src/fieldLib.cpp
@@ -15,7 +15,7 @@ using namespace std;
  */
 int main(void)
 {
-    baseObjField field(5, 5);
+    baseObjField field(7, 5);
 
     for(int i = 0; i < field.objs.size(); i++) {
         printf("position: %2d, data: %s\n", i, field.objs[i].str().c_str()); 
