56d415da24093826f4aed8ac027386fed334a47a
damicha <damicha>
2011-08-22 19:26:37 +0200

    member m (baseMaterial) was replaced by member data (dataObject) that contains m

--- src/objFieldEntry.h
+++ src/objFieldEntry.h
@@ -30,17 +30,42 @@ public:
     objFieldEntry *y_prev, *y_next;
     /* position in field */
     int pos_x, pos_y;
+#if 0
     // FIXME: add object layer: (objFieldEntry.obj: obj.m, obj...)
     baseMaterial *m;    /*!< the object material. A virtual class is used */
-
+#endif
     dataObject *data;   //!< Data field that contains properties and states
 
 public:
     /* constructor */
     objFieldEntry() : pos_x(-1), pos_y(-1),
                       x_prev(NULL), x_next(NULL),
-                      y_prev(NULL), y_next(NULL) {};
+                      y_prev(NULL), y_next(NULL),
+                      data(NULL) {};
 
+
+    /*!
+     *\brief  create a data object
+     */
+    void createDataObject(baseMaterialConfig::materialType_t type)
+    {
+
+        data = new dataObject(type);
+#if 0
+        baseMaterial *m = createMaterial(type);
+        data->type = m;
+#endif
+    }
+
+    void deleteDataObject()
+    {
+        if (data != NULL) {
+            delete data;
+        }
+    }
+
+
+#if 0
     /*! \brief  create a material object
      *  \todo   use copy contructor
      * FIXME: move to dataObject class
@@ -64,7 +89,7 @@ public:
             delete m;
         }
     }
-
+#endif
     /*
      * functions to get the object data as a string 
      */
