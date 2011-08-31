c875199e1af419149f35a61132a47f3082fec63f
damicha <damicha>
2011-08-31 20:36:47 +0200

    Info structure of the player object class was added. The object field entry array is now implemented as a simple array

--- src/objField.h
+++ src/objField.h
@@ -43,13 +43,22 @@ public:
         array       //!< print the field contnet as a formated 2d array
     } stringOutputFormat_t;
 
+    /*!
+     * \struct  player object info
+     */
+    typedef struct {
+        objFieldEntry *obj;     /*!< reference to the player object */
+        int x, y;               /*!< current position in the object field */
+    } playerObjectInfo_t;
+
+
 
 /* ======== class attributes ======== */
 public:
     int size_x;     /*!< size of the field dimension x */
     int size_y;     /*!< size of the field dimension y */
-    deque<objFieldEntry> objs;    /*!< field objects FIXME: use a simple array */
-
+    objFieldEntry       *objs;   /*!< pointer to an array of the field objects */
+    playerObjectInfo_t  plInfo;  /*!< infos about the player object */
 
 /* ======== class init functions ======== */
 public:
@@ -59,26 +68,34 @@ public:
      * \param   size_x field size of the dimension x
      * \param   size_y field size of the dimension y
      */
-    objField(int size_x, int size_y)
+    objField(int size_x, int size_y) : objs(NULL)
     {
-        // set dimensions
+        /* set dimensions */
         this->size_x = size_x;
         this->size_y = size_y;
         
-        // initialze field with an empty default object
-        objFieldEntry defaultObj;
-        objs.resize(size_x * size_y, defaultObj);
-        
-        // do final field object initialization
-        initObj();
+        /* get memory for the field entries */
+        objs = new objFieldEntry[size_x*size_y];
 
-        // create materials (sand surrounded with walls)
+        /* do final field object initialization */
+        initObj();
+        
+
+        /* create materials (sand surrounded with walls)
+         * place the player into the left top corner */
         for (int y = 0; y < size_y; y++)
         {
             for (int x = 0; x < size_x; x++)
             {
-                if (x == 0 || x == size_x - 1 ||
-                    y == 0 || y == size_y - 1)
+                if (x == 1 && y == 1)
+                {
+                    /* set player position/object */
+                    objs[y*size_x + x].createDataObject(baseDataObjectType::player);
+                    plInfo.obj  = &objs[y*size_x + x];
+                    plInfo.x    = x;
+                    plInfo.y    = y;
+                } else if (x == 0 || x == size_x - 1 ||
+                           y == 0 || y == size_y - 1)
                 {
                     objs[y*size_x + x].createDataObject(baseDataObjectType::wall);
                 } else {
@@ -88,40 +105,67 @@ public:
         }
     };
 
-
     /*!
      * \brief   Constructor
      * \details Initializes the field with the given configuration.
      * \param   cfg configuration
+     * FIXME: move function implementation to *.cpp
      */
     objField(const config &cfg)
     {
-        // set dimensions
+        /* set dimensions */
         this->size_x = cfg.size_x;
         this->size_y = cfg.size_y;
         
-        // initialze field with an empty default object
-        objFieldEntry defaultObj;
-        objs.resize(size_x * size_y, defaultObj);
+        /* get memory for the field entries */
+        objs = new objFieldEntry[size_x*size_y];
         
-        // do final field object initialization
+        /* do final field object initialization */
         initObj();
 
-        // create materials
-        // FIXME move material conversion to a material class or somewhere else
+        /* create materials */
+        // FIXME do type conversion in function of the config class
+        // FIXME move player info data and functions to player class dotPlayer
         for (int y = 0; y < size_y; y++)
         {
             for (int x = 0; x < size_x; x++)
             {
-                switch (cfg.data[y*size_x + x]) {
-                    case '#': objs[y*size_x + x].createDataObject(baseDataObjectType::wall); break;
-                    case '.': objs[y*size_x + x].createDataObject(baseDataObjectType::sand); break;
-                    case ' ': objs[y*size_x + x].createDataObject(baseDataObjectType::empty); break;
-                    case 'O': objs[y*size_x + x].createDataObject(baseDataObjectType::stone); break;
+                switch (cfg.data[y*size_x + x])
+                {
+                    case '#':
+                        objs[y*size_x + x].createDataObject(baseDataObjectType::wall);
+                        break;
+                    case '.':
+                        objs[y*size_x + x].createDataObject(baseDataObjectType::sand);
+                        break;
+                    case ' ':
+                        objs[y*size_x + x].createDataObject(baseDataObjectType::empty);
+                        break;
+                    case 'O':
+                        objs[y*size_x + x].createDataObject(baseDataObjectType::stone);
+                        break;
+                    case '8':
+                        /* set player position/object */
+                        objs[y*size_x + x].createDataObject(baseDataObjectType::player);
+                        plInfo.obj  = &objs[y*size_x + x];
+                        plInfo.x    = x;
+                        plInfo.y    = y;
+                        break;
+                    default:
+                        objs[y*size_x + x].createDataObject(baseDataObjectType::unknown);
+                        break;
                 }
             }
         }
     };
+    
+    /*!
+     * \brief   Destructor
+     */
+    ~objField() {
+        delete [] objs;   
+    }
+
 
     /*!
      * \brief   Initialize the field objects.
@@ -156,7 +200,7 @@ public:
         string s;
 
         printf("Field dimensions: (x: %d, y: %d) - %d\n", size_x, size_y, s.max_size());
-        for(int i = 0; i < objs.size(); i++) {
+        for(int i = 0; i < size_x*size_y; i++) {
             printf("position: %2d, data: %s\n", i, objs[i].str().c_str());
         }
         return s;
