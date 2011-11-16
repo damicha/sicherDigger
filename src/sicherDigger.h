18dc15122804c61eeaef6223aa630c18ab0276ae
damicha <damicha>
2011-11-16 21:05:37 +0100

    initial commit. Includes the doxygen main page

---
+++ src/sicherDigger.h
@@ -0,0 +1,40 @@
+/******************************************************************************/
+/*!
+ * \file    sicherDigger.h
+ * \brief   Contains the doxygen main page with a project description
+ *
+ * \author  damicha of defect
+ * \date    2011
+ *
+ * \license See license file in the main directory.
+ *
+ ******************************************************************************/
+
+#ifndef _SDIG_SICHER_DIGGER_H_
+#define _SDIG_SICHER_DIGGER_H_
+
+/*!
+
+\mainpage   Project sicherDigger
+ 
+\section    Introduction
+The project sicherDigger is a variant of the game BoulderDash that was very
+popular in the mid 80's. It's programmed in C++ and currently runnable in a
+shell. It's planned to implement a physic engine with simple rules to control
+the level elements like rocks and monsters and it's planned to implement a SDL
+gfx engine. The sources are managed by a git repository that is accessible on
+GitHub.
+
+The current implementation provides a level with stones and sand surrounded
+by walls. A player can be moved and stones are falling down if the place under
+them is empty.
+
+
+\section    Source
+www.github.com/...FIXME
+
+*/
+
+
+
+#endif // _SDIG_SICHER_DIGGER_H_
