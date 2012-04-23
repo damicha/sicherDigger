33a53e8d0cafaafe25f954e5425775e0c2696b9c
damicha <damicha>
2012-04-23 19:59:09 +0200

    LevelConfig source was splitted in *.cpp and *.h

--- src/LevelConfig.h
+++ src/LevelConfig.h
@@ -1,8 +1,8 @@
 /******************************************************************************/
 /*!
  * \file    LevelConfig.h
- * \brief   Stores the configuration of a single game level as an array of
- *          characters
+ * \brief   Declaration of the LevelConfig class to store level
+ *  configuration data.
  * 
  * \author  damicha of defect
  * \date    2012
@@ -19,15 +19,13 @@
 #include <string>
 #include <string.h>     // memcpy
 
-using namespace std;
-
-
 namespace SDig {
 
 /*!
  * \class   LevelConfig
  * \brief   Is able to store a configuration of a single game level as an
- *          array of characters.
+ *  array of characters and the game's configuration data like time limit,
+ *  sizes, etc.
  */
 class LevelConfig
 {
@@ -43,64 +41,17 @@ private:
 
 /* ======== class init functions ======== */
 public:
-    /*!
-     * \brief   Constructor
-     * \details Set level sizes, create and initialize the level data.
-     * \param[in] pName       The name of the level.
-     * \param[in] pInfo       Level information string.
-     * \param[in] pTimeLimit  Time limit of the level.
-     * \param[in] pSandReq    Required amount of eaten sand to open the exit.
-     * \param[in] pSizeX      Level size of the dimension x.
-     * \param[in] pSizeY      Level size of the dimension y.
-     * \param[in] pData       Character array with the level configuration.
-     */
+    /* Constructor */
     LevelConfig(const string &pName, const string &pInfo,
                 int pTimeLimit, int pSandReq,
                 int pSizeX, int pSizeY,
-                const char *pData) :
-        /* set configuration names and limits */
-        mName(pName), mInfo(pInfo), mTimeLimit(pTimeLimit), mSandReq(pSandReq),
-        /* set array dimensions */
-        mSizeX(pSizeX), mSizeY(pSizeY)
-    {
-        /* create configuration array */
-        mData = new char[mSizeX * mSizeY];
+                const char *pData);
 
-        /* check and copy */
-        // FIXME: check
-        int cnt = 0;
-        for (int y = 0; y < mSizeY; y++) {
-            for (int x = 0; x < mSizeX; x++) {
-                mData[cnt] = pData[cnt];
-                cnt++;
-            }
-        }
-    }
+    /* Copy Constructor */
+    LevelConfig(const LevelConfig &pLevelConfig);
 
-    /*!
-     * \brief   Copy Constructor
-     */
-    LevelConfig(const LevelConfig &pLevelConfig) :
-        /* set configuration names and limits */
-        mName(pLevelConfig.mName), mInfo(pLevelConfig.mInfo),
-        mTimeLimit(pLevelConfig.mTimeLimit), mSandReq(pLevelConfig.mSandReq),
-        /* set array dimensions */
-        mSizeX(pLevelConfig.mSizeX), mSizeY(pLevelConfig.mSizeY)
-    {
-        /* create a new configuration array */
-        mData = new char[mSizeX * mSizeY];
-
-        /* copy */
-        memcpy(mData, pLevelConfig.mData, sizeof(char) * mSizeX * mSizeY);
-    }
-
-    /*!
-     * \brief   Destructor
-     */
-    ~LevelConfig() {
-        /* destroy the level data array */
-        delete [] mData;
-    }
+    /* Destructor */
+    ~LevelConfig();
     
 
 public:
@@ -136,32 +87,9 @@ public:
         return mSizeY;
     }
 
-    /*!
-     * \brief   Get the type of the object that is stored on position X, Y
-     *  in the level configuration array.
-     * \param[in]   pX, pY  x,y coordinates
-     */
-    DOT::Type getData(int pX, int pY) const
-    {
-        switch (mData[pY*mSizeX + pX])
-        {
-            case '#':
-                return DOT::T_WALL;
-            case '.':
-                return DOT::T_SAND;
-            case ' ':
-                return DOT::T_EMPTY;
-            case 'O':
-                return DOT::T_STONE;
-            case '8':
-                /* set player position/object */
-                return DOT::T_PLAYER;
-            case 'E':
-                return DOT::T_EXIT;
-            default:
-                return DOT::T_UNKNOWN;
-        }
-     }
+    /* Get the type of the object that is stored on position X, Y
+     * in the level configuration array. */
+    DOT::Type getData(int pX, int pY) const;
 
 };
 
